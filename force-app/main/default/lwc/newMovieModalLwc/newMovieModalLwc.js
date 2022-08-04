9e14ca9a7954f45a81fd77311d2b8016e543d5d9
Oualid Slaaouiter <oslaaouiter@salesfoce.com>
2022-08-04 15:39:02 +0100

    star & get picklist not workinf

--- force-app/main/default/lwc/newMovieModalLwc/newMovieModalLwc.js
+++ force-app/main/default/lwc/newMovieModalLwc/newMovieModalLwc.js
@@ -1,16 +1,43 @@
-import { api, LightningElement, track } from 'lwc';
+import { api, LightningElement, track, wire } from 'lwc';
+import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi'; 
+import CATEGORY_FIELD from '@salesforce/schema/Movie__c.Category__c'; 
+import MOVIE_OBJECT from '@salesforce/schema/Movie__c';
+
+
 export default class NewMovieModalLWC extends LightningElement {
-    showModal=false;
+    showModal = false;
+
+    @wire(getObjectInfo, { objectApiName: MOVIE_OBJECT })
+    movieMetadata;
+
+    @wire(
+        getPicklistValues, 
+        { recordTypeId: '012000000000000AAA', fieldApiName: CATEGORY_FIELD }
+    )
+    picklistValues;
 
     @track
-    movie = {}
+    types = [];
+
+    @track
+    movie = {
+        Name: '',
+        Category__c: '',
+        Description__c:'',
+        Rating__c:0
+    }
 
     @api
-    show(){
+    show() {
         this.showModal = true;
     }
 
-    cancel(){
+    cancel() {
         this.showModal = false;
+    }
+
+    handleRatingChanged(event) {
+        console.log("rating ->" + event.detail.rating);
+        this.movie.Rating__c = event.detail.rating;
     }
 }
